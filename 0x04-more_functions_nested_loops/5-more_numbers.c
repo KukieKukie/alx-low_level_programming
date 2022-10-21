71de08a1d863a788318acc5835b14cc44f87356f
kukiekukie <nbosomtwesam21@gmail.com>
2022-10-21 16:18:17 -0700

    task 5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - print numbers from 0to 14, ten times
+ * more_numbers - print numbers from 0 to 14, ten times
  * Only use _putchar 3 times
  *
  * Return: output of function
