d82e277618731e44eb5fc424019b53d5f25caf37
kukiekukie <nbosomtwesam21@gmail.com>
2022-10-27 09:49:43 -0700

    0x06 task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_trouper - convert lowercase characters to uppercase
+ *
+ * @s: string
+ *
+ * Return: uppercase characters
+ */
+char *string_trouper(char *s)
+{
+	int index;
+
+	index = 0;
+
+	/* iterate chars in string and check length */
+	while (*(s + index) != '\0')
+	{
+		if (*(s + index) >= 97 && *(s + index) <= 122) /* 97 = a, 122 = z */
+		{
+			*(s + index) -= 97 - 65; /* small case char to uppercase char */
+		}
+		index++;
+	}
+	return (s); /* print to screen uppercase string */
+}
