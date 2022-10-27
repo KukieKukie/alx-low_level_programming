db1449f966a366af3732632be82177d37431b415
kukiekukie <nbosomtwesam21@gmail.com>
2022-10-27 08:08:54 -0700

    0x06 task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * strcat - links the string pointed to by @src, including the terminat
- *  null byte, to the end of the string pointed to by :dest.
- *  @dest: A pointer to the string to be linked upon
- *  @src: The source string to be appended to @dest
+ * strcat - links two strings
+ *  @dest: first string (destination)
+ *  @src: second string (source)
  *
- *  Return: A pointer to the destination string @dest
+ *  Return: Always return 0
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	char *s = dest;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (s);
 }
