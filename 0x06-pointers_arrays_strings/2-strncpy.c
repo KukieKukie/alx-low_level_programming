b6b7e977364c13981872d20921aeaee3572b8249
kukiekukie <nbosomtwesam21@gmail.com>
2022-10-28 20:19:40 -0700

    0x06 task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncopy - copies a string
+ * _strncpy - copies a string
  *
  * @n: length of number of characters to copy
  * @dest: destination string
@@ -9,20 +9,19 @@
  *
  * Return: copied string
  */
-char *_strncopy(char *dest, char *src, int n)
+char _strncopy(char *dest, char *src, int n)
 {
 	int index;
 
 	/* find length, compare strings */
-	for (; index = 0; index < n && *(src + index); index++)
+	for (index = 0; index < n && *(src + index); index++)
 	{
 		*(dest + index) = *(src + index);
 	}
-
 	/* normal length of destination string, ending in null char */
-	for (index < n; index++;)
+	for (; index < n; index++)
 	{
 		*(dest + index) = '\0';
 	}
-	return (dest); /* copied string */
+	return (dest); /* copird string */
 }
